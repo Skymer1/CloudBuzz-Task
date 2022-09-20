819616269376c38f68f32bbbc6426cea78b43905
Skymer1 <108739174+Skymer1@users.noreply.github.com>
2022-09-20 21:02:25 +0300

    Delete data.tf

--- Terraform/data.tf
+++
@@ -1,6 +0,0 @@
-data "aws_iam_policy" "role_api" {
-  arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
-}
-data "aws_iam_policy" "role_sns" {
-  arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
-}
